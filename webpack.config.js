ba6a5a8f1c8f97f0e0a679786f818115a4fcdfd3
StefanYohansson <sy.fen0@gmail.com>
2016-12-15 18:34:37 -0300

    Added webpack plugins and bundle now has ~80kb

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   entry: "./lib/sz-json-rpc",
   output: {
@@ -7,6 +9,12 @@ module.exports = {
     filename: "sz-json-rpc.js",
     umdNamedDefine: true
   },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.OldWatchingPlugin(),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true, sourceMap: true })
+  ],
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
